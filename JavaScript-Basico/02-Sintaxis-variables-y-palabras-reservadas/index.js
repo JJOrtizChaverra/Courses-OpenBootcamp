27aed299168f41b18f0f15143798d5c5d10a97f4
JJOrtizChaverra <juanjochaverra02@gmail.com>
2023-03-06 13:21:00 -0500

    Ejercicio realizado.

---
+++ JavaScript-Basico/02-Sintaxis-variables-y-palabras-reservadas/index.js
@@ -0,0 +1,15 @@
+let miFechaNacimiento = new Date("6 november 2004");
+
+let lista = [
+    "Juan Jose", 
+    18, 
+    false, 
+    miFechaNacimiento,
+    libro = {
+        titulo: "Stranger Things. Mundos al revés",
+        autor: "Gina McIntyre",
+        fecha: "21 de mayo del 2019",
+        url: "https://www.amazon.com/-/es/Gina-McIntyre/dp/8417338683"
+    }
+];
+console.log(lista);
